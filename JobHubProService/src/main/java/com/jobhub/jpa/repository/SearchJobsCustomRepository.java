892ded8b84f968c7ec4ec019a0fa9641df6412f0
Ranjith Ramasamy <ranjith.ramasamy@hotmail.com>
2018-11-11 07:10:21 +0530

    Create job search repositories and integrated with UI

--- JobHubProService/src/main/java/com/jobhub/jpa/repository/SearchJobsCustomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jobhub.jpa.repository;
-
-import java.util.List;
-
-import com.jobhub.jpa.entity.Jobs;
-
-public interface SearchJobsCustomRepository {
-	public List<Jobs> findAllMatchingJobs();
-}
